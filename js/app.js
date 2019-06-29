9e2ad06709931cadef6321015e66fddca02cbc1a
Preston <preston.alex.carter@gmail.com>
2019-06-29 10:11:06 -0500

    Init classes/constructors

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+/******************************************
+Treehouse Techdegree:
+FSJS Project 4 - OOP Game App
+Developer: Preston Carter
+https://github.com/preston-carter
+app.js
+******************************************/
+
+ //Wait to run the script until the content has fully loaded.
+ $(function() {
+
+ /***
+    Global variables that store the DOM elements to reference and/or manipulate.
+ ***/
+//const game = new Game();
+//const phrase = new Phrase();
+const phrase = new Phrase('Life is like a box of chocolates');
+console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+
+});
